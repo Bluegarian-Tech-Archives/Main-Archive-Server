8e3257318f47e06ae8220909bca048745d6133f3
Bluegarian-Tech-Archives <62402965+Bluegarian-Tech-Archives@users.noreply.github.com>
2020-03-19 16:28:24 -0400

    First commit

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+deathMsg="WARNING: YOU HAVE BEEN DETECTED ATTEMPTING TO ACCESS THE FILES ON THIS ARCHIVE WITHOUT OFFICIAL APRROVAL. MEMATIC KILL AGENT ACTIVATED. SAY YOUR LAST WORDS.";
+
+console.log(deathMsg);
+
+function myFunction() {
+    var x = document.getElementById("showRest");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
