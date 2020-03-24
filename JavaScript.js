39361cbcd9b480302f3b180b932fe37284bf5a09
Emperor of Bluegaria <62402965+Bluegarian-Tech-Archives@users.noreply.github.com>
2020-03-24 13:30:09 -0400

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -2,11 +2,6 @@ deathMsg="WARNING: YOU HAVE BEEN DETECTED ATTEMPTING TO ACCESS THE FILES ON THIS
 
 console.log(deathMsg);
 
-function myFunction() {
-    var x = document.getElementById("showRest");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+function spamYee() {
+console.log('YEEEEEEEEEEEEEEEEEEEEEE')
+}
