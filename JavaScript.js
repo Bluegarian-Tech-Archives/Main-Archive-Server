d23c13f7b4539915a6afebe75baedf53e5d74577
Emperor of Bluegaria <62402965+Bluegarian-Tech-Archives@users.noreply.github.com>
2020-03-25 19:36:33 -0400

    Added memetic kill agent

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,14 @@
-deathMsg="WARNING: YOU HAVE BEEN DETECTED ATTEMPTING TO ACCESS THE FILES ON THIS ARCHIVE WITHOUT OFFICIAL APRROVAL. MEMATIC KILL AGENT ACTIVATED. SAY YOUR LAST WORDS.";
+deathMsg="WARNING: YOU HAVE BEEN DETECTED ATTEMPTING TO ACCESS THE FILES ON THIS ARCHIVE WITHOUT OFFICIAL APRROVAL. MTF TEAM DEPLOYED. SAY YOUR LAST WORDS.";
 
 console.log(deathMsg);
 
 function spamYee() {
 console.log('YEEEEEEEEEEEEEEEEEEEEEE')
+}
+
+// <button onclick="spamYee()">Click Me to Spam YEEEEEEEEE in Console</button>
+
+function Reveal() {
+    var rev = document.getElementById("Reveal Archive");
+    rev.style.display = "block";  // <-- Set it to block
 }
