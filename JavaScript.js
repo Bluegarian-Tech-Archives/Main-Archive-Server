fc79a0799969e8dac4cdccd669362634a5df24db
Emperor of Bluegaria <62402965+Bluegarian-Tech-Archives@users.noreply.github.com>
2020-04-02 19:49:50 -0400

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -1,17 +1,33 @@
+//Display in console
 deathMsg="WARNING: YOU HAVE BEEN DETECTED ATTEMPTING TO ACCESS THE FILES ON THIS ARCHIVE WITHOUT OFFICIAL APRROVAL. MTF TEAM DEPLOYED. SAY YOUR LAST WORDS.";
 
 console.log(deathMsg);
 
-function spamYee() {
-console.log('YEEEEEEEEEEEEEEEEEEEEEE');
-}
-
-// <button onclick="spamYee()">Click Me to Spam YEEEEEEEEE in Console</button>
-
+//Reavel Archive if function "Reveal" is triggered by onClick
 function Reveal() {
-    alert('Authorization Success');
+    alert('Verification Success');
 
-    var rev = document.getElementById("Reveal Archive");
+    let rev = document.getElementById("Reveal Archive");
     rev.style.display = "block";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+function spamYee() {
+console.log('YEEEEEEEEEEEEEEEEEEEEEE');
+// <button onclick="spamYee()">Click Me to Spam YEEEEEEEEE in Console</button>
+}
+
+
+
+
 
